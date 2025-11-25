0a2e11acc5660a9914876e78ee558c017fffc0a3
Tinilinsk <120578910+Tinilinsk@users.noreply.github.com>
2025-11-25 21:28:29 +0100

    Added nav buttons for profile page

--- app/src/main/java/com/example/testmap/ProfileActivity.kt
+++ app/src/main/java/com/example/testmap/ProfileActivity.kt
@@ -1,11 +1,24 @@
 package com.example.testmap
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        findViewById<LinearLayout>(R.id.nav_map).setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<LinearLayout>(R.id.nav_home).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
